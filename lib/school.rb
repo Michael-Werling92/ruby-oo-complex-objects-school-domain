acb914a7afa274dff3bb3d208f8aa80f6cb57610
Michael <mfw6mw@virginia.edu>
2021-03-04 01:18:06 -0500

    commit tree 9aa5006122e4686ffc5c8d361b35bdc8a92b0edc

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+    def initialize(name)
+        @name=name
+        @roster={ }
+    end
+    def roster
+        @roster
+    end
+    def add_student(student, grade)
+        if (roster[grade])
+            roster[grade]<< student
+        else
+            roster[grade]=[]
+            roster[grade] << student
+        end
+    roster
+    end
+    def grade(grade)
+        roster[grade]
+    end
+    def sort
+        roster.each do |key, value|
+            value.sort!
+        end
+        roster.sort.to_h
+    end
+end
